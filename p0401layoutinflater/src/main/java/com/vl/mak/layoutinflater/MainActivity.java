c4b21e2aa04383eb8971a8afeef423817f8c6b64
MakSharonov <mak.sharonov@gmail.com>
2016-05-23 22:21:23 +1000

    finished lesson 40

--- p0401layoutinflater/src/main/java/com/vl/mak/layoutinflater/MainActivity.java
+++ p0401layoutinflater/src/main/java/com/vl/mak/layoutinflater/MainActivity.java
@@ -21,22 +21,22 @@ public class MainActivity extends AppCompatActivity {
 
         LayoutInflater ltInflater = getLayoutInflater();
 
-        //LinearLayout linLayout = (LinearLayout) findViewById(R.id.linLayout);
-        //RelativeLayout relLayout = (RelativeLayout) findViewById(R.id.relLayout);
+        LinearLayout linLayout = (LinearLayout) findViewById(R.id.linLayout);
+        RelativeLayout relLayout = (RelativeLayout) findViewById(R.id.relLayout);
 
-        //View view1 = ltInflater.inflate(R.layout.text, linLayout, false);
-        //ViewGroup.LayoutParams lp1 = view1.getLayoutParams();
+        View view1 = ltInflater.inflate(R.layout.text, linLayout, false);
+        ViewGroup.LayoutParams lp1 = view1.getLayoutParams();
 
-        //Log.d(LOG_TAG, "Class of view1: " + view1.getClass().toString());
-        //Log.d(LOG_TAG, "Class of layoutParams of view1: " + lp1.getClass().toString());
+        Log.d(LOG_TAG, "Class of view1: " + view1.getClass().toString());
+        Log.d(LOG_TAG, "Class of layoutParams of view1: " + lp1.getClass().toString());
         //Log.d(LOG_TAG, "Text of view1: " + ((TextView) view1).getText());
 
-        View view2 = ltInflater.inflate(R.layout.text, (ViewGroup) findViewById(R.id.relLayout), true);
+        View view2 = ltInflater.inflate(R.layout.text, relLayout, true);
         ViewGroup.LayoutParams lp2 = view2.getLayoutParams();
 
         Log.d(LOG_TAG, "Class of view2: " + view2.getClass().toString());
         Log.d(LOG_TAG, "Class of layoutParams of view2: " + lp2.getClass().toString());
-        Log.d(LOG_TAG, "Text of view2: " + ((TextView) view2).getText());
+        //Log.d(LOG_TAG, "Text of view2: " + ((TextView) view2).getText());
 
     }
 }
