39ca756a6686f1d5cdda803ff8ca94acbab7a0ba
MakSharonov <mak.sharonov@gmail.com>
2016-06-16 22:35:46 +1000

    lesson 51 finished

--- p0511simpleadapterdata/src/main/java/com/vl/mak/simpleadapterdata/MainActivity.java
+++ p0511simpleadapterdata/src/main/java/com/vl/mak/simpleadapterdata/MainActivity.java
@@ -2,6 +2,10 @@ package com.vl.mak.simpleadapterdata;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
@@ -30,6 +34,46 @@ public class MainActivity extends AppCompatActivity {
         data = new ArrayList<Map<String, Object>>();
         for (int i=1; i<5; i++) {
             m = new HashMap<String, Object>();
+            m.put(ATTRIBUTE_NAME_TEXT, "sometext " + i);
+            m.put(ATTRIBUTE_NAME_IMAGE, R.drawable.lo);
+            data.add(m);
         }
+
+        String[] from = new String[] { ATTRIBUTE_NAME_TEXT, ATTRIBUTE_NAME_IMAGE };
+        int[] to = new int[] { R.id.tvText, R.id.ivImg };
+
+        adapter = new SimpleAdapter(this, data, R.layout.item, from, to);
+
+        lvSimple = (ListView) findViewById(R.id.lvSimple);
+        lvSimple.setAdapter(adapter);
+        registerForContextMenu(lvSimple);
+    }
+
+    public void onButtonClick(View v) {
+        m = new HashMap<String, Object>();
+        m.put(ATTRIBUTE_NAME_TEXT, "sometext " + (data.size() + 1));
+        m.put(ATTRIBUTE_NAME_IMAGE, R.drawable.lo);
+        data.add(m);
+
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.add(0, CM_DELETE_ID, 0, "удалить");
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        if (item.getItemId() == CM_DELETE_ID) {
+            AdapterView.AdapterContextMenuInfo acme = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+            data.remove(acme.position);
+            adapter.notifyDataSetChanged();
+            return true;
+        }
+
+        return super.onContextItemSelected(item);
     }
 }
