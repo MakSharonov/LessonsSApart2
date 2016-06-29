60d6112d8bc8f35f441a8c750d9cfaf720f8e3ac
MakSharonov <mak.sharonov@gmail.com>
2016-06-30 00:16:37 +1000

    lesson 64

--- p0641alertdialogitemsmulti/src/main/java/com/vl/mak/alertdialogitemsmulti/MainActivity.java
+++ p0641alertdialogitemsmulti/src/main/java/com/vl/mak/alertdialogitemsmulti/MainActivity.java
@@ -1,4 +1,4 @@
-package com.vl.mak.alertdialogitemssingle;
+package com.vl.mak.alertdialogitemsmulti;
 
 import android.app.Dialog;
 import android.content.DialogInterface;
@@ -7,8 +7,8 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.SparseBooleanArray;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,12 +16,12 @@ public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = "ma_log";
 
     final int DIALOG_ITEMS = 1;
-    final int DIALOG_ADAPTER = 2;
-    final int DIALOG_CURSOR = 3;
+    final int DIALOG_CURSOR = 2;
     DB db;
     Cursor cursor;
 
     String data[] = { "one", "two", "three", "four" };
+    boolean chkd[] = { false, true, true, false };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,13 +36,10 @@ public class MainActivity extends AppCompatActivity {
 
     public void onclick(View v) {
         switch (v.getId()) {
-            case(R.id.btnItems):
+            case R.id.btnItems:
                 showDialog(DIALOG_ITEMS);
                 break;
-            case(R.id.btnAdapter):
-                showDialog(DIALOG_ADAPTER);
-                break;
-            case(R.id.btnCursor):
+            case R.id.btnCursor:
                 showDialog(DIALOG_CURSOR);
                 break;
         }
@@ -54,30 +51,43 @@ public class MainActivity extends AppCompatActivity {
         switch (id) {
             case (DIALOG_ITEMS):
                 adb.setTitle(R.string.items);
-                adb.setSingleChoiceItems(data, -1, myClickListener);
-                break;
-            case (DIALOG_ADAPTER):
-                adb.setTitle(R.string.adapter);
-                ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.select_dialog_singlechoice, data);
-                adb.setSingleChoiceItems(adapter, -1, myClickListener);
+                adb.setMultiChoiceItems(data, chkd, myItemsMultiClickListener);
                 break;
             case (DIALOG_CURSOR):
                 adb.setTitle(R.string.cursor);
-                adb.setSingleChoiceItems(cursor, -1, DB.COLUMN_TXT, myClickListener);
+                adb.setMultiChoiceItems(cursor, DB.COLUMN_CHK, DB.COLUMN_TXT, myCursorMultiClickListener);
                 break;
         }
         adb.setPositiveButton(R.string.ok, myClickListener);
         return adb.create();
     }
 
+    AlertDialog.OnMultiChoiceClickListener myItemsMultiClickListener = new DialogInterface.OnMultiChoiceClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+            Log.d(LOG_TAG, "which = " + which + " is checked = " + isChecked);
+        }
+    };
+
+    AlertDialog.OnMultiChoiceClickListener myCursorMultiClickListener = new DialogInterface.OnMultiChoiceClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+            ListView lv = ((AlertDialog) dialog).getListView();
+            Log.d(LOG_TAG, "which = " + which + " is checked = " + isChecked);
+            //db.ChangeRec(which, isChecked);
+            cursor.requery();
+        }
+    };
+
     AlertDialog.OnClickListener myClickListener = new DialogInterface.OnClickListener() {
         @Override
         public void onClick(DialogInterface dialog, int which) {
-            ListView lv = ((AlertDialog) dialog).getListView();
-            if (which == Dialog.BUTTON_POSITIVE) {
-                Log.d(LOG_TAG, "pos = " + lv.getCheckedItemPosition());
-            } else {
-                Log.d(LOG_TAG, "which = " + which);
+            SparseBooleanArray sba = ((AlertDialog)dialog).getListView().getCheckedItemPositions();
+            for (int i=0; i<sba.size(); i++) {
+                int key = sba.keyAt(i);
+                if (sba.get(key)) {
+                    Log.d(LOG_TAG, "checked: " + key);
+                }
             }
         }
     };
