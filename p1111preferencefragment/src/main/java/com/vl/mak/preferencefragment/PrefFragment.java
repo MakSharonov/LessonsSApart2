e8bfd293a95b322f881dfd99e32c3e2120f14c0c
Mak <mak.sharonov@gmail.com>
2018-04-18 00:04:49 +1000

    lesson 111 draft

---
+++ p1111preferencefragment/src/main/java/com/vl/mak/preferencefragment/PrefFragment.java
@@ -0,0 +1,24 @@
+package com.vl.mak.preferencefragment;
+
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class PrefFragment extends PreferenceFragment {
+
+
+    public PrefFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.pref1);
+    }
+}
